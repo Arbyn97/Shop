2bcda4b8dadb68836eeef60b3cc1aa70a40e61ee
Arbyn97 <88933674+Arbyn97@users.noreply.github.com>
2026-09-06 21:06:06 +0330

    Create AddcartButton.tsx

---
+++ components/AddcartButton.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { CartContext } from "@/context/CartContext";
+import { useContext } from "react";
+import { Product } from "@/types/product";
+type Props = {
+  data: Product;
+};
+function AddCartButton({data}:Props) {
+    
+    const {setCart}=useContext(CartContext)
+    
+    const hndleclickAdd=()=>{
+     
+        
+        // setCart(prev => [...prev, data])
+         setCart(prev => [...prev, {...data,count:1} ])
+        
+    }
+    return ( 
+        <>
+         <button onClick={hndleclickAdd} className="bg-pink-300 text-pink-50 hover:text-pink-600 hover:bg-white hover:border-2 hover:border-pink-500 text-lg p-3 rounded-xl cursor-pointer">افزودن به سبد خرید</button>
+        </>
+     );
+}
+
+export default AddCartButton;
