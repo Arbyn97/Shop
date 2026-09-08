48aa182081b33b34ea9b04a9b00aa3b8239a9659
Arbyn97 <88933674+Arbyn97@users.noreply.github.com>
2026-09-08 20:49:06 +0330

    Update page.tsx cart/payment

--- app/(shop)/cart/payment/page.tsx
+++ app/(shop)/cart/payment/page.tsx
@@ -1,39 +1,46 @@
 'use client'
+
+import { useEffect, useState } from "react";
+
 function Payment() {
-    const id=localStorage.getItem("userID")
-    const payTotal=async(e: React.FormEvent<HTMLFormElement>)=>{
-         
+    const [id, setId] = useState<string | null>(null);
+
+    useEffect(() => {
+        const userID = localStorage.getItem("userID");
+        setId(userID);
+    }, []);
+
+    const payTotal = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const data=await fetch("/api/invoices",{
-            method:"PUT",
-            body:JSON.stringify({
-                userID:Number(id),
-                status:"paid"
+        const data = await fetch("/api/invoices", {
+            method: "PUT",
+            body: JSON.stringify({
+                userID: Number(id),
+                status: "paid"
             })
-        })
-        
-        console.log(data)
+        });
 
+        console.log(data);
 
-         if(data.ok){
-          console.log("پرداخت انجام شد")  
-         }
-    }
-   
+        if (data.ok) {
+            console.log("پرداخت انجام شد");
+        }
+    };
+
     return (
         <>
-        <form method="POST" onSubmit={payTotal}>
-            <input type="text" placeholder="name"/>
-            <input type="text" placeholder="last name"/>
-            <input type="text" placeholder="phone"/>
-            
-            <button type="submit">
-                پرداخت
-            </button>
-        </form>
+            <form method="POST" onSubmit={payTotal}>
+                <input type="text" placeholder="name" />
+                <input type="text" placeholder="last name" />
+                <input type="text" placeholder="phone" />
+
+                <button type="submit">
+                    پرداخت
+                </button>
+            </form>
         </>
-      );
+    );
 }
 
 export default Payment;
