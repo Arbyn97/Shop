01775c0bf198b5ff532c354d2c73813e8e0c31f4
Arbyn97 <88933674+Arbyn97@users.noreply.github.com>
2026-09-14 18:40:30 +0330

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,21 +1,59 @@
+
 import { NextResponse } from "next/server";
-import users from '@/data/users.json'
-import fs from "fs/promises";
-import path from"path"
-const filepath=path.join(process.cwd(),"data","users.json")
+import { supabase } from "@/lib/supabase";
 
-
+// GET: دریافت همه کاربران
 export async function GET() {
-    return NextResponse.json(users)
-    
-};
+  const { data, error } = await supabase
+    .from("users")
+    .select("*");
 
-export async function POST(request: Request) {
-  
-  const newUser = await request.json();
-  const file =await fs.readFile(filepath,"utf-8")
-  const user=JSON.parse(file)
-  user.push(newUser)
-  await fs.writeFile(filepath,JSON.stringify(user,null,2))
-  return NextResponse.json(newUser);
+  if (error) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(data);
 }
+
+
+// POST: ایجاد کاربر جدید
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { id, name, email, password, role } = body;
+
+    const { data, error } = await supabase
+      .from("users")
+      .insert([
+        {
+          id,
+          name,
+          email,
+          password,
+          role: role || "user",
+        },
+      ])
+      .select()
+      .single();
+
+    if (error) {
+      return NextResponse.json(
+        { error: error.message },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json(data, { status: 201 });
+
+  } catch {
+    return NextResponse.json(
+      { error: "اطلاعات ارسال شده صحیح نیست" },
+      { status: 400 }
+    );
+  }
+}
+
